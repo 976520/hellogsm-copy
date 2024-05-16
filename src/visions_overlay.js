25ffdfcd612296d1a33f69b9e35fc96bf6ced3f3
송재욱 <123460320+976520@users.noreply.github.com>
2024-05-16 22:38:46 +0900

    img

--- src/visions_overlay.js
+++ src/visions_overlay.js
@@ -4,9 +4,8 @@ const upCover = document.getElementById('up');
 const saYongJa = document.createElement('sa-young-ja');
 const needs = document.createElement('needs');
 const jeoHei = document.createElement('jeo-hei');
-    
-const container = document.getElementById('cover-container');
 
+const container = document.getElementById('cover-container');
 
 window.addEventListener('mousemove', up);
 window.addEventListener('scroll', up);
@@ -15,23 +14,28 @@ window.addEventListener('mousemove', down);
 window.addEventListener('scroll', down);
 window.addEventListener('wheel', down);
 
-
 function up(event) {
-    let clientY = event.clientY;
-    let containerHeight = upCover.parentElement.clientHeight;
-    let maxHeight = 700;
+  let clientY = event.clientY;
+  let containerHeight = upCover.parentElement.clientHeight;
+  let maxHeight = 700;
 
-    let height = (clientY - upCover.parentElement.getBoundingClientRect().top) / containerHeight * maxHeight;
-    upCover.style.height = height + 'px';
+  let height =
+    ((clientY - upCover.parentElement.getBoundingClientRect().top) /
+      containerHeight) *
+    maxHeight;
+  upCover.style.height = height + 'px';
 }
 
 function down(event) {
-    let clientY = event.clientY;
-    let containerHeight = container.clientHeight;
-    let maxHeight = 700;
+  let clientY = event.clientY;
+  let containerHeight = container.clientHeight;
+  let maxHeight = 700;
 
-    let height = maxHeight - ((clientY - container.getBoundingClientRect().top) / containerHeight * maxHeight);
+  let height =
+    maxHeight -
+    ((clientY - container.getBoundingClientRect().top) / containerHeight) *
+      maxHeight;
 
-    downCover.style.height = height + 'px';
-    downCover.style.top = 700 - height + 'px';
+  downCover.style.height = height + 'px';
+  downCover.style.top = 700 - height + 'px';
 }
